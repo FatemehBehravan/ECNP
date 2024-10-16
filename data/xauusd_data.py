2cbb85610148f9c16aa7e77f934e8662cedb3d4a
FatemehBehravan <elahebehraavaan@gmail.com>
2024-10-16 07:30:56 +0000

    new_change

--- data/xauusd_data.py
+++ data/xauusd_data.py
@@ -21,7 +21,8 @@ class NumericDataset(object):
                  x_size=1,
                  y_size=1,
                  testing=False,
-                 device="cpu"):
+                 device="cpu",
+                 ):
 
         self._batch_size = batch_size
         self._max_num_context = max_num_context
@@ -29,6 +30,8 @@ class NumericDataset(object):
         self._y_size = y_size
         self._testing = testing
         self._device=device
+        self.start_index = 0
+        
 
 
 
@@ -40,53 +43,105 @@ class NumericDataset(object):
             data = pd.read_csv(file_path)
             data.dropna(subset=['date', 'open', 'close'], inplace=True)
             data['date'] = pd.to_datetime(data['date'], errors='coerce')
+            data = data[data.date > '2024-01-01']
             data['date'] = data['date'].astype(int) / 10**9
-            data['open'] = data['open'].astype(int)
+            #data['open'] = data['open'].astype(int)
             data['colse'] = data['close'].astype(int)
-            X = data['date'].values
+            
+            x = data['date'].values
             y = data['close'].values
-            return X, y
+            return x, y
 
         
         base_path = 'datasets/XAUUSD/'
-        X_train, y_train = load_csv_data(os.path.join(base_path, 'train/train_data.csv'))
-        X_val, y_val = load_csv_data(os.path.join(base_path, 'val/val_data.csv'))
-        X_test, y_test = load_csv_data(os.path.join(base_path, 'test/test_data.csv'))
+        x_train, y_train = load_csv_data(os.path.join(base_path, 'train/train_data.csv'))
+        x_val, y_val = load_csv_data(os.path.join(base_path, 'val/val_data.csv'))
+        x_test, y_test = load_csv_data(os.path.join(base_path, 'test/test_data.csv'))
 
 
-        feature_columns = [ 'date']
-        label_columns = ['close']
-
+        # feature_columns = [ 'date']
+        # label_columns = ['close']
 
         if fixed_num_context>0:
             num_context = fixed_num_context
         else:
             num_context = torch.randint(low=3, high=self._max_num_context + 1, size=(1,))
 
-
+        window_size = 96
+        step = 16
+        
         if self._testing:
-            num_total_points = 400
+            num_total_points = 50
             num_target = num_total_points
-            x_values = torch.tensor(X_test[:self._batch_size * num_total_points], dtype=torch.float32)
-            y_values = torch.tensor(y_test[:self._batch_size * num_total_points], dtype=torch.float32)
-            x_values = x_values.view(self._batch_size, num_total_points, self._x_size)  # x_size=1
-            y_values = y_values.view(self._batch_size, num_total_points, self._y_size)  # y_size=1
+            
+            # print(self.start_index)
+            if self.start_index + window_size > len(x_test):
+                print("End of data reached")
+                self.start_index = 0
+            else:
+                
+                window_data_x = x_test[self.start_index:self.start_index + window_size]
+                window_data_y = y_test[self.start_index:self.start_index + window_size]
+
+                if len(window_data_x) < self._batch_size * num_total_points:
+                    raise ValueError(f"Not enough data points: expected {self._batch_size * num_total_points}, got {len(window_data_x)}")
+
+                
+                x_values = torch.tensor(window_data_x[:self._batch_size * num_total_points], dtype=torch.float32)
+                y_values = torch.tensor(window_data_y[:self._batch_size * num_total_points], dtype=torch.float32)
+
+                x_values = x_values.view(self._batch_size, num_total_points, self._x_size) 
+                y_values = y_values.view(self._batch_size, num_total_points, self._y_size)  
+                self.start_index += step
+
+                  
+
+
+            # x_values = torch.tensor(x_test[:self._batch_size * num_total_points], dtype=torch.float32)
+            # y_values = torch.tensor(y_test[:self._batch_size * num_total_points], dtype=torch.float32)
+            # x_values = x_values.view(self._batch_size, num_total_points, self._x_size)  # x_size=1
+            # y_values = y_values.view(self._batch_size, num_total_points, self._y_size)  # y_size=1
 
             # print(x_values.shape)
             # print(y_values.shape)
 
         else:
-            num_target = torch.randint(3,self._max_num_context+1, size = (1,))
+            num_target = 2
             num_total_points = num_target + num_context
-            x_values = torch.tensor(X_train[:self._batch_size * num_total_points], dtype=torch.float32)
-            y_values = torch.tensor(y_train[:self._batch_size * num_total_points], dtype=torch.float32)
-            x_values = x_values.view(self._batch_size, num_total_points, self._x_size)  # x_size=1
-            y_values = y_values.view(self._batch_size, num_total_points, self._y_size)  # y_size=1
+            
+            if self.start_index + window_size > len(x_train):
+                print("End of data reached")
+                self.start_index = 0
+            else:
+                # print('first_step_train')
+                window_data_x = x_train[self.start_index:self.start_index + window_size]
+                window_data_y = y_train[self.start_index:self.start_index + window_size]
+
+                if len(window_data_x) < self._batch_size * num_total_points:
+                    raise ValueError(f"Not enough data points: expected {self._batch_size * num_total_points}, got {len(window_data_x)}")
+
+                
+                x_values = torch.tensor(window_data_x[:self._batch_size * num_total_points], dtype=torch.float32)
+                y_values = torch.tensor(window_data_y[:self._batch_size * num_total_points], dtype=torch.float32)
+
+                x_values = x_values.view(self._batch_size, num_total_points, self._x_size) 
+                y_values = y_values.view(self._batch_size, num_total_points, self._y_size)  
+                self.start_index += step
+
+
+
+
+            # x_values = torch.tensor(x_train[:self._batch_size * num_total_points], dtype=torch.float32)
+            # y_values = torch.tensor(y_train[:self._batch_size * num_total_points], dtype=torch.float32)
+            # x_values = x_values.view(self._batch_size, num_total_points, self._x_size)  # x_size=1
+            # y_values = y_values.view(self._batch_size, num_total_points, self._y_size)  # y_size=1
             # print(x_values.shape)
             # print(y_values.shape)
 
         task_property = torch.tensor(1)
+
         if self._testing:
+            
             target_x = x_values
             target_y = y_values
             # print('here',x_values.shape)
@@ -95,6 +150,7 @@ class NumericDataset(object):
             context_x = x_values[:, idx[:num_context], :]
             context_y = y_values[:,idx[:num_context],:]
         else:
+            # print('secound_step_train')
             target_x = x_values[:,:num_target+num_context,:]
             target_y = y_values[:,:num_target+num_context,:]
 
